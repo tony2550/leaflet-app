abc9f5bb88767d5e894d8062e237c6053055cebc
김도현 <mookieum1991@gmail.com>
2022-04-19 12:44:51 +0900

    first leaflet map

---
+++ README.md
@@ -0,0 +1,2 @@
+# leaflet-app
+# leaflet-app
---
+++ script.js
@@ -0,0 +1,10 @@
+let map = L.map('map', { center: [35.17, 129.07], zoom: 13 });
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'myTO',
+}).addTo(map);
